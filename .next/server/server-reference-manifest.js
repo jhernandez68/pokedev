d3116f38e32136e67f507fb6da5c8cfa59fa4f76
jhernandez68 <jhoanstiven123xd@gmail.com>
2025-07-12 18:17:38 -0500

    feat: docs

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8THVZEWXmuqYMx9lfNDF1Rb7CRD3KdizpLH8xKxXzVA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_contact_page_tsx_007ca514._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_72e51f74._.js"
-],
-    source: "dynamic"
-});
